d2b9efa3bfec8ee4bb1a5c6e66831397fbc7469c
Yasutomi-Mizuki-Tokushima <yasutomianabuki@gmail.com>
2018-08-06 12:17:26 +0900

    boss追加

--- program/main(α1)/boss.h
+++ program/main(α1)/boss.h
@@ -12,13 +12,13 @@ const float BOSS_SPEED = 0.0f;
 using namespace std;
 
 enum BossType{
-	body, leftarm, rightarm, lefhand, righand
+	body = 3, rightarm, righand, leftarm, lefhand
 };
 
 class CBossData : public CBaseData{
 public:
 	CBossData();
-	//座標		・生きてる		・角度		・大きさ	・アニメーション	・速度			・質量		・体力			・摩擦	・当たり判定の大きさ	・種類
+	//座標		・生きてる		・角度		・大きさ	・アニメーション	・速度			・質量	・体力		・摩擦	・当たり判定の大きさ	・種類
 	CBossData(CVector2D _pos, bool _living, float _rad, float _exrate, int _animtype, float _velocity, float _mass, int _hp, float _friction, float _collision, int _type);
 
 	//座標		・生きてる		・透過値		・角度		・大きさ	・アニメーション	・速度			・質量		・体力			・摩擦	・当たり判定の大きさ	・種類
@@ -26,25 +26,32 @@ public:
 	CBossData(CBaseData _temp);
 	~CBossData(){};
 	float m_yup = 0;
+	float m_x, m_y;
+	int m_tyep;
 };
 
 class CBoss : public CTask{
 private:
-	vector<CBossData> m_bo;
-	list<CBossData*> m_bos;
+	list<CBossData*> m_boss;
 	CBossData m_bossy;
-	float i;
+	float a;
+	float b;
 	int m_boss_img[5];
 	int m_boss_body_img;
 	int m_boss_leftarm_img;
 	int m_boss_rightarm_img;
-	int u = 0;
+	int m_count;			//出現用カウンター
+	bool u;
+	bool m_attack_flag;
+	CBaseData *_temp;
 public:
 	CBoss();
 	~CBoss(){};
 	void Update();
 	void Move(CBossData &cd, CVector2D &_pos);
+	void Movestart(CBossData &cd, CVector2D &_pos, bool loop, int type);
 	void Draw();
+	list<CBossData*> *GetBossData(){ return &m_boss; };
 	//vector<CBossData> *GetBossData(){ return &m_bo; };
 };
 
